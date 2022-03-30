3994f6fec570fb21af04f27770da429041c50c80
Matias Aguilar <mac.bol2011@gmail.com>
2022-03-30 17:53:10 -0400

    NewVersion

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>feriaDelAlto.com</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
